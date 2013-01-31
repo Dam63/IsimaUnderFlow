bfcb2b8440d8dfb4ddc980570d5120e380025bce
Damien VESSIERE <damien.vessiere@gmail.com>
2013-02-01 00:28:03 +0100

    Minor Fix : import String builder remove

--- grails-app/taglib/fr/isima/isimaUnderFlow/TagsTagLib.groovy
+++ grails-app/taglib/fr/isima/isimaUnderFlow/TagsTagLib.groovy
@@ -1,7 +1,5 @@
 package fr.isima.isimaUnderFlow
 
-import com.sun.xml.internal.ws.client.sei.ResponseBuilder.StringBuilder;
-
 class TagsTagLib {
 	def questionMini = { attrs, body ->
 		out << body() << render(template: "/tagsLib/questionMini", model: [question: attrs.question]);//"<div><div></div>" //(attrs.happy == 'true' ? " :-)" : " :-(")
